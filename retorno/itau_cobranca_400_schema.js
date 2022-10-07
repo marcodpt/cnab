24aad203e2f491be12be5272c2f25677725c34b0
Marco Di Pillo Tomic <marcodpt@protonmail.com>
2022-10-07 18:30:52 -0300

    schema para o retorno

---
+++ retorno/itau_cobranca_400_schema.js
@@ -0,0 +1,28 @@
+export default {
+  "title": "Retorno Itaú Cobrança",
+  "description": "Layout para retorno de cobrança Itaú CNAB 400",
+  "type": "object",
+  "properties": {
+    "tipo": {},
+    "servico": {},
+    "banco": {},
+    "agencia": {},
+    "conta": {},
+    "digito": {},
+    "empresa": {},
+    "geracao": {},
+    "sequencia": {},
+    "credito": {},
+    "indice": {},
+    "registros": {
+      "type": "array",
+      "minItems": 1,
+      "items": {
+        "type": "object",
+        "properties": {
+
+        }
+      }
+    }
+  }
+}
